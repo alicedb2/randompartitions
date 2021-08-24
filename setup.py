1f66bab2bb72b69173d97fcff6955c8af10e252f
alicedb2 <alice.doucet.beaupre@gmail.com>
2021-08-24 11:06:12 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='randompartitions',
+      version='0.1',
+      description='Random integer partition generator using algorithm 5 of Arratia & DeSalvo',
+      url='http://github.com/alicedb2/randompartitions',
+      author='Alice Doucet Beaupre',
+      author_email='alice.doucet.beaupre@gmail.com',
+      license='MIT',
+      packages=['randompartitions'],
+      zip_safe=False)
